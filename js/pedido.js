9fac8b3381b84c304e71e6aca167c1740f9f2853
BeatingHeartMS <49695336+BeatingHeartMS@users.noreply.github.com>
2023-11-13 01:14:23 -0300

    Update pedido.js

--- js/pedido.js
+++ js/pedido.js
@@ -36,7 +36,9 @@ function cargarEventos() {
 }
 
 async function fetchProductos() {
-	let res = await fetch("/data/productos.json");
+	
+	let res = await fetch("https://raw.githubusercontent.com/msangalli1/ProyectoBreadConcept/main/data/productos.json");
+	// let res = await fetch("/data/productos.json");
 	let data = await res.json();
 	let html = "";
 	data.forEach((producto, index) => {
